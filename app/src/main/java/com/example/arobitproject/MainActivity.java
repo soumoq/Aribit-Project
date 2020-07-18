db6b102430d16404baf5e69d167c35000619e853
root <soumojyotiganguly270@gmail.com>
2020-07-19 01:48:30 +0530

    create welcome and login screen

--- app/src/main/java/com/example/arobitproject/MainActivity.java
+++ app/src/main/java/com/example/arobitproject/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.arobitproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView truckImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_main);
+
+        truckImage = findViewById(R.id.truck_image);
+        truckImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SigningInActivity.class));
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/arobitproject/SigningInActivity.java
@@ -0,0 +1,23 @@
+package com.example.arobitproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.Window;
+import android.widget.TextView;
+
+public class SigningInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        setContentView(R.layout.activity_signin);
+
+
+
+    }
+}
