d433f334299296921ddeb3be4ac6fd742122f594
root <soumojyotiganguly270@gmail.com>
2020-07-19 03:16:45 +0530

    create otp activity

--- app/src/main/java/com/example/arobitproject/OtpActivity.java
+++ app/src/main/java/com/example/arobitproject/OtpActivity.java
@@ -3,12 +3,15 @@ package com.example.arobitproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class OtpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_otp);
     }
 }
--- app/src/main/java/com/example/arobitproject/SigningInActivity.java
+++ app/src/main/java/com/example/arobitproject/SigningInActivity.java
@@ -2,13 +2,18 @@ package com.example.arobitproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SigningInActivity extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,12 @@ public class SigningInActivity extends AppCompatActivity {
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_signin);
 
-
-
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SigningInActivity.this,OtpActivity.class));
+            }
+        });
     }
 }
