d5716f4881cb316ff8c204b0fe2dade01421475a
root <soumojyotiganguly270@gmail.com>
2020-07-30 03:59:36 +0530

    add OrderDetailsActivity

--- app/src/main/java/com/example/arobitproject/DashBordListView.java
+++ app/src/main/java/com/example/arobitproject/DashBordListView.java
@@ -3,11 +3,13 @@ package com.example.arobitproject;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -23,10 +25,17 @@ public class DashBordListView extends ArrayAdapter<String> {
 
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
-        @SuppressLint("ViewHolder") View rowView = inflater.inflate(R.layout.dashboard_list_view, null, true);
+        @SuppressLint("ViewHolder") final View rowView = inflater.inflate(R.layout.dashboard_list_view, null, true);
 
         TextView customerName = rowView.findViewById(R.id.customer_name);
+        TextView details = rowView.findViewById(R.id.details_user);
         customerName.setText(customer_name[position]);
+        details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rowView.getContext().startActivity(new Intent(rowView.getContext(), OrderDetailsActivity.class));
+            }
+        });
         return rowView;
     }
 }
