a83577096678d3ecd038530f9cfbea94e0b809ab
root <soumojyotiganguly270@gmail.com>
2020-07-30 14:58:21 +0530

    fix otp box

--- app/src/main/java/com/example/arobitproject/OtpActivity.java
+++ app/src/main/java/com/example/arobitproject/OtpActivity.java
@@ -4,11 +4,14 @@ package com.example.arobitproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.EditorInfo;
@@ -18,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.chaos.view.PinView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
@@ -31,18 +35,14 @@ import java.util.concurrent.TimeUnit;
 
 public class OtpActivity extends AppCompatActivity {
 
-    private EditText otpText1;
-    private EditText otpText2;
-    private EditText otpText3;
-    private EditText otpText4;
-    private EditText otpText5;
-    private EditText otpText6;
+
     private TextView phoneNoTextView;
     private FirebaseAuth mAuth;
     private String verificationId;
     private ImageView backArrow;
     private String phoneNo;
     private MyKeyboard keyboard;
+    private PinView pinview;
 
 
     @Override
@@ -53,25 +53,11 @@ public class OtpActivity extends AppCompatActivity {
 
         keyboard = (MyKeyboard) findViewById(R.id.keyboard);
         backArrow = findViewById(R.id.back_arrow);
-        otpText1 = findViewById(R.id.et_otp1);
-        otpText2 = findViewById(R.id.et_otp2);
-        otpText3 = findViewById(R.id.et_otp3);
-        otpText4 = findViewById(R.id.et_otp4);
-        otpText1.setInputType(InputType.TYPE_NULL);
-        otpText2.setInputType(InputType.TYPE_NULL);
-        otpText3.setInputType(InputType.TYPE_NULL);
-        otpText4.setInputType(InputType.TYPE_NULL);
-
-
-        inputKey(otpText1);
 
 
         phoneNoTextView = findViewById(R.id.phone_no_text_view);
 
-        otpText1.addTextChangedListener(new GenericTextWatcher(otpText1));
-        otpText2.addTextChangedListener(new GenericTextWatcher(otpText2));
-        otpText3.addTextChangedListener(new GenericTextWatcher(otpText3));
-        otpText4.addTextChangedListener(new GenericTextWatcher(otpText4));
+
         phoneNo = "7477540540";
         phoneNoTextView.setText("Please enter the verification\ncode sent to " + phoneNo);
 
@@ -83,76 +69,30 @@ public class OtpActivity extends AppCompatActivity {
             }
         });
 
+        final PinView pinView = findViewById(R.id.pin_view);
+        pinView.setRawInputType(InputType.TYPE_CLASS_DATETIME);
+        pinView.setHideLineWhenFilled(false);
+        pinView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(pinView.getText().toString().length() == 4)
+                {
+                    startActivity(new Intent(OtpActivity.this,HomeActivity.class));
+                    pinView.setText(null);
+                }
+                return false;
+            }
+        });
+        //inputKey(pinView);
 
     }
 
-    private void inputKey(EditText otpText) {
+    private void inputKey(final EditText otpText) {
         otpText.setRawInputType(InputType.TYPE_CLASS_TEXT);
         otpText.setTextIsSelectable(true);
         InputConnection ic = otpText.onCreateInputConnection(new EditorInfo());
+
         keyboard.setInputConnection(ic);
-    }
-
-
-    public class GenericTextWatcher implements TextWatcher {
-        private View view;
-
-        GenericTextWatcher(View view) {
-            this.view = view;
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-            // TODO Auto-generated method stub
-            String text = editable.toString();
-            switch (view.getId()) {
-
-                case R.id.et_otp1:
-                    if (text.length() == 1) {
-                        otpText2.requestFocus();
-                        inputKey(otpText2);
-                    }
-                    break;
-                case R.id.et_otp2:
-                    if (text.length() == 1) {
-                        otpText3.requestFocus();
-                        inputKey(otpText3);
-                    } else if (text.length() == 0) {
-                        otpText1.requestFocus();
-                        inputKey(otpText1);
-                    }
-                    break;
-                case R.id.et_otp3:
-                    if (text.length() == 1) {
-                        otpText4.requestFocus();
-                        inputKey(otpText4);
-                    } else if (text.length() == 0) {
-                        otpText2.requestFocus();
-                        inputKey(otpText2);
-                    }
-                    break;
-                case R.id.et_otp4:
-                    if (text.length() == 0) {
-                        otpText3.requestFocus();
-                        inputKey(otpText3);
-                    } else if (text.length() == 1) {
-                        startActivity(new Intent(OtpActivity.this, HomeActivity.class));
-                        //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                        finish();
-                    }
-                    break;
-            }
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
-            // TODO Auto-generated method stub
-        }
-
-        @Override
-        public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
-            // TODO Auto-generated method stub
-        }
     }
 
 }
